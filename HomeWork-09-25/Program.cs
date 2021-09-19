1d90e5731e8d4d128feadd72818ab4cc1c41b6f7
muradkerimov <muradik@code.edu.az>
2021-09-20 03:32:12 +0400

    Add project files.

---
+++ HomeWork-09-25/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace HomeWork_09_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task-1
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Console.Write("Balınızı daxil edin: ");
+            //        int score = Convert.ToInt32(Console.ReadLine());
+            //        if (score <= 100 && score > 90)
+            //        {
+            //            Console.WriteLine("A");
+            //        }
+            //        else if (score <= 90 && score > 80)
+            //        {
+            //            Console.WriteLine("B");
+            //        }
+            //        else if (score <= 80 && score > 70)
+            //        {
+            //            Console.WriteLine("C");
+            //        }
+            //        else if (score <= 70 && score > 60)
+            //        {
+            //            Console.WriteLine("D");
+            //        }
+            //        else if (score <= 60 && score > 50)
+            //        {
+            //            Console.WriteLine("E");
+            //        }
+            //        else if (score <= 50 && score > 0)
+            //        {
+            //            Console.WriteLine("kesr");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("1 ile 100 arasinda eded daxil edin");
+            //        }
+            //        break;
+            //    }
+            //    catch (Exception)
+            //    {
+            //        Console.WriteLine("1 ile 100 arasinda eded daxil edin");
+            //    }
+            //}
+            #endregion
+
+            #region Task-8
+            //Console.Write("reqem daxil edin: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //switch (number)
+            //{
+            //    case 1:
+            //        Console.WriteLine("winter");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("spring");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("summer");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("autom");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("1 ile 4 arasinda reqem daxil edin");
+            //        break;
+            //}
+            #endregion
+        }
+    }
+}
